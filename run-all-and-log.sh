ff8e5b99bd0e69df6fda782a389d565a194dc32d
Mao <m@maowtm.org>
2022-08-06 23:22:48 +0100

    Make run-all-and-log.sh take an argument

--- run-all-and-log.sh
+++ run-all-and-log.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
+# Usage: ./run-all-and-log.sh [N]
+# where N is the number of times to repeat the experiment
+# default is 100
+
 set -x
 mkdir runlog || true
-for i in $(seq 1 100); do
+N=${1:-100}
+for i in $(seq 1 $N); do
   for j in ubuntu redis node mysql traefik nginx; do
     ./run-all.py $j > runlog/$i-$j.log
     sleep 1
